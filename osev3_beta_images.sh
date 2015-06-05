fc07e2c24df12154a6cf0f8ae360cd2647400411
root <root@ghoelzer-osev3-beta3-mstr01b.os1.phx2.redhat.com>
2015-06-05 17:02:39 -0400

    Added Docker Image Pull shell script

---
+++ osev3_beta_images.sh
@@ -0,0 +1,13 @@
+#Pull Docker Images needed for Beta & Labs
+docker pull registry.access.redhat.com/openshift3_beta/ose-haproxy-router:v0.4.3.2
+docker pull registry.access.redhat.com/openshift3_beta/ose-deployer:v0.4.3.2
+docker pull registry.access.redhat.com/openshift3_beta/ose-sti-builder:v0.4.3.2
+docker pull registry.access.redhat.com/openshift3_beta/ose-docker-builder:v0.4.3.2
+docker pull registry.access.redhat.com/openshift3_beta/ose-pod:v0.4.3.2
+docker pull registry.access.redhat.com/openshift3_beta/ose-docker-registry:v0.4.3.2
+docker pull registry.access.redhat.com/openshift3_beta/sti-basicauthurl:latest
+#Labs
+docker pull registry.access.redhat.com/openshift3_beta/ruby-20-rhel7
+docker pull registry.access.redhat.com/openshift3_beta/mysql-55-rhel7
+docker pull openshift/hello-openshift:v0.4.3
+docker pull openshift/ruby-20-centos7
